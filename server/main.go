8bac55c7820f1a009db90b2b0dc0b6bb4015cae9
Dicky Wahyudi Pratama <4katamudahuntukemail@gmail.com>
2022-01-26 10:09:38 +0700

    Last update = finally connected to postgress

--- server/main.go
+++ server/main.go
@@ -1,22 +1,62 @@
 package main
+
 import (
-	"github.com/gin-gonic/gin"
- 	"net/http"
+	"log"
+	"os"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-func main() {
-	router := gin.Default()
+type Person struct{
+	gorm.Model
 
-	router.GET("/", home)
-
-	router.Run(":8888")
+	Name string
+	Email string `gorm:"typevarchar(100); unique_index"`
+	Book []Book
 
 }
 
-func home(c *gin.Context){
-	c.JSON(http.StatusOK, gin.H{
-		"title": "Home",
-		"subtitle": "This is homepage",
-	})
+type Book struct{
+	gorm.Model
+
+	Title string
+	Author string
+	BookId int `gorm:"typevarchar(100); "unique_index"`
+	PersonID int
+}
+
+var db *gorm.DB
+var err error
+
+func main() {
+	// Access ENV var
+	dialect := os.Getenv("DIALECT")
+	host := os.Getenv("HOST")
+	dbport := os.Getenv("DBPORT")
+	user := os.Getenv("USER")
+	dbname := os.Getenv("NAME")
+	password := os.Getenv("PASSWORD")
+
+	//DB CONNECT string
+	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s  port=%s ", host, user, dbname, password, dbport)
+
+	//OPEN COnnection to db
+
+	db, err := gorm.Open(dialect, dbURI)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Connected to database")
+	}
+
+	//close connection
+	defer db.Close()
+
+	//make migrations to the db if not exist
+	db.AutoMigrate(&Person{})
+	db.AutoMigrate(&Book{})
+
+
 
 }
